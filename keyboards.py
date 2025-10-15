100b92d50d3fe829bb844d3cb702d0abcd9a2325
Kai 🌸 <kayoosh2009@gmail.com>
2025-10-15 18:00:43 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,6 @@
+from aiogram import types
+
+# Заготовка клавиатур для будущего меню
+KB_LANG = types.ReplyKeyboardMarkup(resize_keyboard=True)
+KB_MENU_RU = types.ReplyKeyboardMarkup(resize_keyboard=True)
+KB_MENU_EN = types.ReplyKeyboardMarkup(resize_keyboard=True)
