173af942de9b535b3d5e24d05637285bd963414a
Kai 🌸 <kayoosh2009@gmail.com>
2025-10-16 16:46:47 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from aiogram import executor
-from loader import dp
-import handlers.upload  # подключаем обработчик загрузки треков
-
-async def on_startup(dp):
-    print("Бот запущен и готов к работе!")
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
