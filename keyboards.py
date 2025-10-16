c8134c1d9296d11aee9f4c248f6266fbfccdc9f0
Kai 🌸 <kayoosh2009@gmail.com>
2025-10-16 16:46:29 +0300

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types
-
-# Заготовка клавиатур для будущего меню
-KB_LANG = types.ReplyKeyboardMarkup(resize_keyboard=True)
-KB_MENU_RU = types.ReplyKeyboardMarkup(resize_keyboard=True)
-KB_MENU_EN = types.ReplyKeyboardMarkup(resize_keyboard=True)
