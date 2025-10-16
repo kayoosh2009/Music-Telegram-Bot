e224b9ab86b1c5b953fd5882cd2f8deb2cad7db5
Kai 🌸 <kayoosh2009@gmail.com>
2025-10-16 16:46:38 +0300

    Delete loader.py

--- loader.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import TELEGRAM_BOT_TOKEN
-
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
